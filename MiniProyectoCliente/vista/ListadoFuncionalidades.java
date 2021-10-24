f858fd6c633cac960133380e7f0e30e972e510a8
Yésica González <88462080+yesicaGonzalez@users.noreply.github.com>
2021-10-24 18:11:46 -0300

    Update ListadoFuncionalidades.java

--- MiniProyectoCliente/vista/ListadoFuncionalidades.java
+++ MiniProyectoCliente/vista/ListadoFuncionalidades.java
@@ -1,4 +1,4 @@
-package Vista;
+package vista;
 
 import java.awt.EventQueue;
 
@@ -6,6 +6,7 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+
 import java.awt.Font;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -17,16 +18,28 @@ import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.table.DefaultTableModel;
 
-import com.entities.Funcionalidad;
-import com.servicios.FuncionalidadBeanRemote;
+import com.exception.ServiciosException;
+import com.servicios.Funcionalidad_BeanRemote;
+import com.servicios.Rol_BeanRemote;
+import com.servicios.Usuario_BeanRemote;
+
+import model.Funcionalidad;
+import model.Rol;
+import model.Usuario;
+
 import javax.swing.JTextField;
-import javax.swing.JCheckBox;
+import java.awt.SystemColor;
+import javax.swing.JTextArea;
 
-public class ListadoFuncionalidades implements MouseListener{
+public class Listado_Funcionalidades implements MouseListener{
 
-	private JFrame frame;
+	JFrame frame;
 	private JTextField textFuncionalidad;
-	private JTextField textDescripcion;
+	private JLabel lblInformacin;
+	private JScrollPane scrollPane_1;
+	private JTextArea textDescripcion;
+	private JTextField textRol;
+	private JTextField textAcceso;
 
 	/**
 	 * Launch the application.
@@ -35,7 +48,7 @@ public class ListadoFuncionalidades implements MouseListener{
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ListadoFuncionalidades window = new ListadoFuncionalidades();
+					Listado_Funcionalidades window = new Listado_Funcionalidades();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -47,7 +60,7 @@ public class ListadoFuncionalidades implements MouseListener{
 	/**
 	 * Create the application.
 	 */
-	public ListadoFuncionalidades() {
+	public Listado_Funcionalidades() {
 		initialize();
 	}
 
@@ -57,17 +70,17 @@ public class ListadoFuncionalidades implements MouseListener{
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.WHITE);
-		frame.setBounds(100, 100, 1002, 550);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(100, 100, 1160, 606);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblListadoFuncionalidades = new JLabel("Listado de Funcionalidades");
 		lblListadoFuncionalidades.setFont(new Font("Baskerville Old Face", Font.ITALIC, 27));
-		lblListadoFuncionalidades.setBounds(10, 20, 373, 31);
+		lblListadoFuncionalidades.setBounds(26, 30, 373, 31);
 		frame.getContentPane().add(lblListadoFuncionalidades);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(26, 71, 577, 419);
+		scrollPane.setBounds(26, 71, 634, 419);
 		frame.getContentPane().add(scrollPane);
 		
 		JTable table = new JTable();
@@ -75,7 +88,16 @@ public class ListadoFuncionalidades implements MouseListener{
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		scrollPane.setViewportView(table);
 		
-		DefaultTableModel model = new DefaultTableModel();
+		DefaultTableModel model = new DefaultTableModel(){
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
 		
 		Object[] columns = {"Nombre", "Descripcion"};
 		
@@ -86,34 +108,94 @@ public class ListadoFuncionalidades implements MouseListener{
 		
 		JLabel lblFuncionalidad = new JLabel("Funcionalidad");
 		lblFuncionalidad.setFont(new Font("Baskerville Old Face", Font.PLAIN, 16));
-		lblFuncionalidad.setBounds(613, 103, 170, 25);
+		lblFuncionalidad.setBounds(670, 125, 170, 25);
 		frame.getContentPane().add(lblFuncionalidad);
 		
 		textFuncionalidad = new JTextField();
+		textFuncionalidad.setEditable(false);
 		textFuncionalidad.setColumns(10);
-		textFuncionalidad.setBounds(613, 133, 352, 25);
+		textFuncionalidad.setBounds(670, 150, 466, 25);
 		frame.getContentPane().add(textFuncionalidad);
 		
 		JLabel lblDescripcin = new JLabel("Descripci\u00F3n");
 		lblDescripcin.setFont(new Font("Baskerville Old Face", Font.PLAIN, 16));
-		lblDescripcin.setBounds(613, 168, 170, 25);
+		lblDescripcin.setBounds(670, 196, 170, 25);
 		frame.getContentPane().add(lblDescripcin);
 		
-		textDescripcion = new JTextField();
-		textDescripcion.setColumns(10);
-		textDescripcion.setBounds(613, 200, 352, 25);
-		frame.getContentPane().add(textDescripcion);
+		lblInformacin = new JLabel("Informaci\u00F3n");
+		lblInformacin.setForeground(SystemColor.activeCaption);
+		lblInformacin.setFont(new Font("Baskerville Old Face", Font.PLAIN, 27));
+		lblInformacin.setBounds(670, 71, 373, 31);
+		frame.getContentPane().add(lblInformacin);
 		
-		JCheckBox CheckAcceso = new JCheckBox("\u00BFTengo acceso?");
-		CheckAcceso.setFont(new Font("Baskerville Old Face", Font.PLAIN, 16));
-		CheckAcceso.setBounds(716, 264, 146, 31);
-		frame.getContentPane().add(CheckAcceso);
+		scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(670, 220, 466, 104);
+		frame.getContentPane().add(scrollPane_1);
+		
+		textDescripcion = new JTextArea();
+		textDescripcion.setWrapStyleWord(true);
+		textDescripcion.setLineWrap(true);
+		textDescripcion.setEditable(false);
+		scrollPane_1.setViewportView(textDescripcion);
+		
+		textRol = new JTextField();
+		textRol.setForeground(new Color(70, 130, 180));
+		textRol.setFont(new Font("Baskerville Old Face", Font.PLAIN, 16));
+		textRol.setBackground(Color.WHITE);
+		textRol.setBorder(null);
+		textRol.setEditable(false);
+		textRol.setColumns(10);
+		textRol.setBounds(805, 430, 182, 25);
+		frame.getContentPane().add(textRol);
+		
+				
+		Usuario user = null;
+		
+		try {
+			String dato = "Mini_ProyectoEJB/Usuario_Bean!com.servicios.Usuario_BeanRemote";
+			Usuario_BeanRemote ad = (Usuario_BeanRemote) InitialContext.doLookup(dato);
+			
+			user = ad.findUser(Login.textCorreo.getText(), String.valueOf(Login.password.getPassword())); 
+			
+			if(user != null) {
+				textRol.setText(user.getRole().getNombre());
+			}
+			
+		} catch(NamingException | ServiciosException ex) {
+			ex.getMessage();
+		}
+		
+		JLabel lblUstedPoseeEl = new JLabel("Usted posee el rol:");
+		lblUstedPoseeEl.setFont(new Font("Baskerville Old Face", Font.PLAIN, 16));
+		lblUstedPoseeEl.setBounds(670, 430, 170, 25);
+		frame.getContentPane().add(lblUstedPoseeEl);
+		
+		JLabel lblAccesoALa = new JLabel("Acceso a la funcionalidad");
+		lblAccesoALa.setForeground(SystemColor.activeCaption);
+		lblAccesoALa.setFont(new Font("Baskerville Old Face", Font.PLAIN, 25));
+		lblAccesoALa.setBounds(670, 372, 373, 31);
+		frame.getContentPane().add(lblAccesoALa);
+		
+		JLabel lblAccesoALa_1 = new JLabel("Acceso a la funcionalidad seleccionada :");
+		lblAccesoALa_1.setFont(new Font("Baskerville Old Face", Font.PLAIN, 16));
+		lblAccesoALa_1.setBounds(670, 465, 260, 25);
+		frame.getContentPane().add(lblAccesoALa_1);
+		
+		textAcceso = new JTextField();
+		textAcceso.setFont(new Font("Baskerville Old Face", Font.PLAIN, 16));
+		textAcceso.setEditable(false);
+		textAcceso.setColumns(10);
+		textAcceso.setBorder(null);
+		textAcceso.setBackground(Color.WHITE);
+		textAcceso.setBounds(940, 465, 182, 25);
+		frame.getContentPane().add(textAcceso);
 		
 		List<Funcionalidad> funcionalidades = new LinkedList<>();
 
 		try {
-			FuncionalidadBeanRemote funcionalidadBean = (FuncionalidadBeanRemote)
-					InitialContext.doLookup("MiniproyectoEJB/FuncionalidadBean!com.servicios.FuncionalidadBeanRemote");
+			Funcionalidad_BeanRemote funcionalidadBean = (Funcionalidad_BeanRemote)
+					InitialContext.doLookup("Mini_ProyectoEJB/Funcionalidad_Bean!com.servicios.Funcionalidad_BeanRemote");
+			
 			funcionalidades = funcionalidadBean.mostrarTodos();
 
 		} catch (NamingException e3) {
@@ -137,6 +219,32 @@ public class ListadoFuncionalidades implements MouseListener{
 				}else {
 					textFuncionalidad.setText(table.getValueAt(filaSeleccionada, 0).toString());
 					textDescripcion.setText(table.getValueAt(filaSeleccionada, 1).toString());
+					
+					String rol = textRol.getText();
+					String funcion = table.getValueAt(filaSeleccionada, 0).toString();
+					
+					Rol r = null;
+					
+					try {
+						String dato = "Mini_ProyectoEJB/Rol_Bean!com.servicios.Rol_BeanRemote";
+						Rol_BeanRemote ro = (Rol_BeanRemote) InitialContext.doLookup(dato);
+							
+						r = ro.findAcceso( rol, funcion);
+						
+						
+						if(r != null) {
+							textAcceso.setText("Verdadero");
+							textAcceso.setForeground(new Color(95, 158, 160));
+							
+						}else {
+							textAcceso.setText("Falso");
+							textAcceso.setForeground(Color.RED);
+						}
+						
+						
+					}catch(NamingException | ServiciosException ex) {
+						ex.getMessage();
+					}
 				}
 			}
 
